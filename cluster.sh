5d4bc5cf991245434d421e4764b5707ae722f368
Robert Schuh <github@eneticum.de>
2021-04-06 00:34:54 +0200

    Update cluster.sh

--- cluster.sh
+++ cluster.sh
@@ -2,7 +2,10 @@
 set -m
 
 EJABBERDCTL=$HOME/bin/ejabberdctl
-EJABBERD_NODE=ejabberd@$(hostname).$HEADLESS_SERVICE
+HOSTNAME_S=$(hostname -s) # ejabberd-0
+HOSTNAME_F=$(hostname -f) # ejabberd-0.ejabberd.default.svc.cluster.local
+HEADLESS_SERVICE="${HOSTNAME_F/$HOSTNAME_S./}" # ejabberd.default.svc.cluster.local
+EJABBERD_NODE="ejabberd@$HOSTNAME_F" # ejabberd@ejabberd-0.ejabberd.default.svc.cluster.local
 
 echo $HOME
 echo $EJABBERDCTL
@@ -15,49 +18,36 @@ echo "launched ejabberd in foreground, sleeping"
 
 sleep 5
 
-OWN_IP=$(nslookup $(hostname).$HEADLESS_SERVICE | tail -n +3 | grep "Address:" | sed -E 's/^Address: (.*)$/\1/')
-HOSTNAMES=$(nslookup $OWN_IP | tail -n +3 | grep -E '[^=]*= (.*).'"$HEADLESS_SERVICE"'$'  | sed -E 's/[^=]*= (.*).'"$HEADLESS_SERVICE"'$/\1/')
-for HOSTNAME in ${HOSTNAMES}
-do
-    OWN_HOSTNAME=$HOSTNAME
-done
-
-
 IPS=$(nslookup $HEADLESS_SERVICE | tail -n +3 | grep "Address:" | sed -E 's/^Address: (.*)$/\1/')
-
 for IP in ${IPS}
 do
-    echo $IP
-    if [[ "$OWN_IP" == "$IP" ]] ; then
-        echo "found own ip, skipping"
+    echo "looking up hostname for: $IP"
+    HOSTNAME=$(nslookup $IP | tail -n +3 | grep -E '[^=]*= (.*).'"$HEADLESS_SERVICE"'$'  | sed -E 's/[^=]*= (.*).'"$HEADLESS_SERVICE"'$/\1/')
+    if [[ "$HOSTNAME_S" == "$HOSTNAME" ]] ; then
+        echo "found own hostname, skipping"
         continue
+    fi
+    echo "trying to connect to node with hostname $HOSTNAME.$HEADLESS_SERVICE"
+    chmod 600 $HOME/.erlang.cookie
+    $EJABBERDCTL join_cluster "ejabberd@$HOSTNAME.$HEADLESS_SERVICE"
+    if [[ $? -eq 0 ]] ; then
+        echo "successfully joined";
+        break
     else
-        echo "found different ip, looking up hostname for: $IP"
-        HOSTNAMES=$(nslookup $IP | tail -n +3 | grep -E '[^=]*= (.*).'"$HEADLESS_SERVICE"'$'  | sed -E 's/[^=]*= (.*).'"$HEADLESS_SERVICE"'$/\1/')
-        for HOSTNAME in ${HOSTNAMES}
-        do
-            echo "trying to connect to node with hostname $HOSTNAME.$HEADLESS_SERVICE"
-            chmod 600 $HOME/.erlang.cookie
-            $EJABBERDCTL join_cluster "ejabberd@$HOSTNAME.$HEADLESS_SERVICE"
-            if [[ $? -eq 0 ]] ; then
-                echo "successfully joined";
-                break
-            else
-                echo "failed to join, trying next";
-            fi
-        done
+        echo "failed to join, trying next";
     fi
 done
 
 chmod 600 $HOME/.erlang.cookie
 $EJABBERDCTL list_cluster
 
-
 #Define cleanup procedure
 cleanup() {
     echo "Container stopped, performing cleanup..."
     chmod 600 $HOME/.erlang.cookie
-    $EJABBERDCTL leave_cluster "ejabberd@$OWN_HOSTNAME.$HEADLESS_SERVICE"
+    $EJABBERDCTL leave_cluster "$EJABBERD_NODE"
+    chmod 600 $HOME/.erlang.cookie
+    $EJABBERDCTL stop
 }
 
 #Trap SIGTERM
