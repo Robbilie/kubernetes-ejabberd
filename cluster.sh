10dbf41535d2f14e7b8dfcfd2bef4bdde0f5e993
Robert Schuh <github@eneticum.de>
2021-04-06 02:29:47 +0200

    Update cluster.sh

--- cluster.sh
+++ cluster.sh
@@ -5,18 +5,13 @@ EJABBERDCTL=$HOME/bin/ejabberdctl
 HOSTNAME_S=$(hostname -s) # ejabberd-0
 HOSTNAME_F=$(hostname -f) # ejabberd-0.ejabberd.default.svc.cluster.local
 HEADLESS_SERVICE="${HOSTNAME_F/$HOSTNAME_S./}" # ejabberd.default.svc.cluster.local
-EJABBERD_NODE="ejabberd@$HOSTNAME_F" # ejabberd@ejabberd-0.ejabberd.default.svc.cluster.local
+ERLANG_NODE_ARG="ejabberd@$HOSTNAME_F" # ejabberd@ejabberd-0.ejabberd.default.svc.cluster.local
+ERLANG_NODE=$ERLANG_NODE_ARG
 
 echo $HOME
 echo $EJABBERDCTL
 echo $HEADLESS_SERVICE
-echo $EJABBERD_NODE
-
-$EJABBERDCTL foreground &
-
-echo "launched ejabberd in foreground, sleeping"
-
-sleep 5
+echo $ERLANG_NODE_ARG
 
 IPS=$(nslookup $HEADLESS_SERVICE | tail -n +3 | grep "Address:" | sed -E 's/^Address: (.*)$/\1/')
 for IP in ${IPS}
@@ -27,9 +22,14 @@ do
         echo "found own hostname, skipping"
         continue
     fi
+    $EJABBERDCTL -n $ERLANG_NODE_ARG start
+    $EJABBERDCTL -n $ERLANG_NODE_ARG started
     echo "trying to connect to node with hostname $HOSTNAME.$HEADLESS_SERVICE"
-    chmod 600 $HOME/.erlang.cookie
-    $EJABBERDCTL join_cluster "ejabberd@$HOSTNAME.$HEADLESS_SERVICE"
+    $EJABBERDCTL -n $ERLANG_NODE_ARG join_cluster "ejabberd@$HOSTNAME.$HEADLESS_SERVICE"
+    CLUSTERING_RESULT=$?
+    $EJABBERDCTL -n $ERLANG_NODE_ARG list_cluster
+    $EJABBERDCTL -n $ERLANG_NODE_ARG stop
+    $EJABBERDCTL -n $ERLANG_NODE_ARG stopped
     if [[ $? -eq 0 ]] ; then
         echo "successfully joined";
         break
@@ -38,20 +38,18 @@ do
     fi
 done
 
-chmod 600 $HOME/.erlang.cookie
-$EJABBERDCTL list_cluster
-
 #Define cleanup procedure
 cleanup() {
     echo "Container stopped, performing cleanup..."
-    chmod 600 $HOME/.erlang.cookie
-    $EJABBERDCTL leave_cluster "$EJABBERD_NODE"
-    chmod 600 $HOME/.erlang.cookie
-    $EJABBERDCTL stop
+    $EJABBERDCTL -n $ERLANG_NODE_ARG leave_cluster "$ERLANG_NODE_ARG"
+    $EJABBERDCTL -n $ERLANG_NODE_ARG stop
 }
 
 #Trap SIGTERM
 trap 'cleanup' SIGTERM
+
+echo "launching in foreground"
+$EJABBERDCTL -n $ERLANG_NODE_ARG foreground &
 
 #Wait
 wait $!
